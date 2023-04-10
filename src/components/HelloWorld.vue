178004af1c87517917d601eb35758eaff1002702
Olang Daniel <olangdan17@gmail.com>
2023-04-10 03:03:47 +0100

    first commit from rebuilding  portfolio

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      Welcome to my personal website 
+      It is presently under reconstruction using vue3.
+      <br>
+      A little about myself i am a software engineer fascinated by new tech and would be sharing my knowledge which revolves around PHP, Javascript, Rust, Golang, Docker, Ansible, Terraform, Kubernetes, Linux and many more
+      <br>
+      I am very excited 
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
